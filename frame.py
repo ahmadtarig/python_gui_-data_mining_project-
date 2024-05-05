75817255f0b7b31dabb6f51a6e1eb60b409a70b8
ahmadtarig <ahmedtarig.email@gmail.com>
2024-05-05 17:30:35 +0300

    light mode and add right panel

--- frame.py
+++ frame.py
@@ -1,8 +1,14 @@
 import customtkinter as tk
+# set mode
+tk.set_appearance_mode("light")
+
 # create frame
 root = tk.CTk()
 root.attributes("-fullscreen", True)
 
+# create a loft panel
+left_panel = tk.CTkFrame(master=root, fg_color="blue", width=200, corner_radius=0)
+left_panel.pack(side="right", padx=0, pady=0, fill="y")
 
 # main loop
 root.mainloop()
