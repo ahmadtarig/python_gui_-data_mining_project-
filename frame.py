427a5d5a998bf0c4e155780aa08aebf669c7c6d5
ahmadtarig <ahmedtarig.email@gmail.com>
2024-05-06 03:40:10 +0300

    add fuzzy fuction

--- frame.py
+++ frame.py
@@ -1,18 +1,80 @@
-from string import whitespace
-
 import customtkinter as tk
+from PIL import Image, ImageTk
 import pandas as pd
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-# mport numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# import data set
 
-# btn functions
+def get_fuzzy_panel():
+    right_panel.pack_forget()
+    panel = tk.CTkScrollableFrame(master=root, fg_color="#a5bcd9", corner_radius=20,
+                                  bg_color="transparent", scrollbar_button_color=left_panel_color)
+    panel.pack(side="left", padx=10, pady=10, fill="both", expand=True)
 
+    image1 = Image.open(r"C:\Users\Ahmed Tarek\OneDrive\Desktop\mining_project\fuzzyVisualization\1.png")
+    photo1 = ImageTk.PhotoImage(image1)
 
-def cleaning1_functions():
+    image2 = Image.open(r"C:\Users\Ahmed Tarek\OneDrive\Desktop\mining_project\fuzzyVisualization\2.png")
+    photo2 = ImageTk.PhotoImage(image2)
+
+    image3 = Image.open(r"C:\Users\Ahmed Tarek\OneDrive\Desktop\mining_project\fuzzyVisualization\3.png")
+    photo3 = ImageTk.PhotoImage(image3)
+
+    image4 = Image.open(r"C:\Users\Ahmed Tarek\OneDrive\Desktop\mining_project\fuzzyVisualization\4.png")
+    photo4 = ImageTk.PhotoImage(image4)
+
+    image_p = Image.open(r"C:\Users\Ahmed Tarek\OneDrive\Desktop\mining_project\fuzzyVisualization\code.png")
+    photo_p = ImageTk.PhotoImage(image_p)
+
+    image5 = Image.open(r"C:\Users\Ahmed Tarek\OneDrive\Desktop\mining_project\fuzzyVisualization\5.png")
+    photo5 = ImageTk.PhotoImage(image5)
+
+    image6 = Image.open(r"C:\Users\Ahmed Tarek\OneDrive\Desktop\mining_project\fuzzyVisualization\6.png")
+    photo6 = ImageTk.PhotoImage(image6)
+
+    image7 = Image.open(r"C:\Users\Ahmed Tarek\OneDrive\Desktop\mining_project\fuzzyVisualization\7.png")
+    photo7 = ImageTk.PhotoImage(image7)
+
+    image8 = Image.open(r"C:\Users\Ahmed Tarek\OneDrive\Desktop\mining_project\fuzzyVisualization\8.png")
+    photo8 = ImageTk.PhotoImage(image8)
+
+    # Create a label and display the image
+    label1 = tk.CTkLabel(panel, image=photo1, text="")
+    label1.pack(side=tk.TOP, fill=tk.BOTH, pady=20)
+
+    label2 = tk.CTkLabel(panel, image=photo2, text="")
+    label2.pack(side=tk.TOP, fill=tk.BOTH, pady=20)
+
+    label3 = tk.CTkLabel(panel, image=photo3, text="")
+    label3.pack(side=tk.TOP, fill=tk.BOTH, pady=20)
+
+    label4 = tk.CTkLabel(panel, image=photo4, text="")
+    label4.pack(side=tk.TOP, fill=tk.BOTH, pady=20)
+
+    label_p = tk.CTkLabel(panel, image=photo_p, text="")
+    label_p.pack(side=tk.TOP, fill=tk.BOTH, pady=20)
+
+    label5 = tk.CTkLabel(panel, image=photo5, text="")
+    label5.pack(side=tk.TOP, fill=tk.BOTH, pady=20)
+
+    label6 = tk.CTkLabel(panel, image=photo6, text="")
+    label6.pack(side=tk.TOP, fill=tk.BOTH, pady=20)
+
+    label7 = tk.CTkLabel(panel, image=photo7, text="")
+    label7.pack(side=tk.TOP, fill=tk.BOTH, pady=20)
+
+    label8 = tk.CTkLabel(panel, image=photo8, text="")
+    label8.pack(side=tk.TOP, fill=tk.BOTH, pady=20)
+
+    return panel
+
+def get_cleaning_panel():
+    # steps panels
+    cleaning_pane = tk.CTkScrollableFrame(master=root, fg_color="#a5bcd9", corner_radius=20,
+                                          bg_color="transparent", scrollbar_button_color=left_panel_color)
+    cleaning_pane.pack(side="left", padx=10, pady=10, fill="both", expand=True)
+    # steps panels
     df = pd.read_csv(r"C:\Users\Ahmed Tarek\OneDrive\Desktop\mining_project\dataSet.csv")
     columns_of_interest = ['Likes', 'Comments', 'Shares']
     fig = plt.figure(figsize=(10, 6), facecolor=right_panel_color)
@@ -43,7 +105,7 @@ def cleaning1_functions():
     plt.ylabel('')
 
     plt.subplots_adjust(wspace=0.7, hspace=0.8)
-    canvas = FigureCanvasTkAgg(fig, master=right_panel)
+    canvas = FigureCanvasTkAgg(fig, master=cleaning_pane)
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH)
 
@@ -73,22 +135,31 @@ def cleaning1_functions():
     plt.title('Correlation Matrix')
 
     plt.subplots_adjust(wspace=0.7, hspace=0.8)
-    canvas = FigureCanvasTkAgg(fig, master=right_panel)
+    canvas = FigureCanvasTkAgg(fig, master=cleaning_pane)
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH)
 
     fig = plt.figure(figsize=(10, 6), facecolor=right_panel_color)
-    sns.scatterplot(data=df, x='Likes', y='Impressions')
+    sns.scatterplot(data=df, x='Likes', y='Impressions', sizes=1)
     plt.title('Likes vs. Impressions')
 
     plt.subplots_adjust(wspace=0.7, hspace=0.8)
-    canvas = FigureCanvasTkAgg(fig, master=right_panel)
+    canvas = FigureCanvasTkAgg(fig, master=cleaning_pane)
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH)
+    return cleaning_pane
+
+
+def get_k_mediod_panel():
+    panel = tk.CTkScrollableFrame(master=root, fg_color="#a5bcd9", corner_radius=20,
+                                  bg_color="transparent", scrollbar_button_color=left_panel_color)
+    panel.pack(side="left", padx=10, pady=10, fill="both", expand=True)
+
+    pass
 
 
 def exit_function():
-    root.destroy()
+    exit()
 
 
 # btn functions
@@ -124,8 +195,7 @@ exit_button.pack(padx=20, pady=0, side="right")
 # top panel component
 
 # create a right panel
-right_panel = tk.CTkScrollableFrame(master=root, fg_color="#a5bcd9", corner_radius=20,
-                                    bg_color="transparent", scrollbar_button_color=left_panel_color)
+right_panel = tk.CTkFrame(master=root, fg_color="#a5bcd9", corner_radius=20, bg_color="transparent")
 right_panel.pack(side="left", padx=10, pady=10, fill="both", expand=True)
 
 # create a loft panel
@@ -138,29 +208,23 @@ steps = tk.CTkLabel(master=left_panel, text="Steps", text_color=right_panel_colo
                     bg_color="transparent", fg_color="transparent", height=40, width=300, corner_radius=20)
 steps.pack(padx=0, pady=30, side="top")
 
-# cleaning1 button
-cleaning1 = tk.CTkButton(master=left_panel, text="Cleaning 1", text_color="white", font=("sanserif", 16, "bold"),
-                         border_width=0, bg_color="transparent", fg_color="transparent", hover_color="#6666ff",
-                         height=40, width=300, corner_radius=10, command=cleaning1_functions)
-cleaning1.place(y=100, x=0)
-
-# cleaning2 button
-cleaning2 = tk.CTkButton(master=left_panel, text="Cleaning 2", text_color="white", font=("sanserif", 16, "bold"),
-                         border_width=0, bg_color="transparent", fg_color="transparent", hover_color="#6666ff",
-                         height=40, width=300, corner_radius=10)
-cleaning2.place(y=145, x=0)
+# cleaning button
+cleaning = tk.CTkButton(master=left_panel, text="Cleaning", text_color="white", font=("sanserif", 16, "bold"),
+                        border_width=0, bg_color="transparent", fg_color="transparent", hover_color="#6666ff",
+                        height=40, width=300, corner_radius=10, command=get_cleaning_panel)
+cleaning.place(y=100, x=0)
 
 # fuzzy_logic button
 fuzzy_logic = tk.CTkButton(master=left_panel, text="Fuzzy logic", text_color="white", font=("sanserif", 16, "bold"),
                            border_width=0, bg_color="transparent", fg_color="transparent", hover_color="#6666ff",
-                           height=40, width=300, corner_radius=10)
-fuzzy_logic.place(y=190, x=0)
+                           height=40, width=300, corner_radius=10, command=get_fuzzy_panel)
+fuzzy_logic.place(y=145, x=0)
 
 # k_medoid button
 k_medoid = tk.CTkButton(master=left_panel, text="K-mediod", text_color="white", font=("sanserif", 16, "bold"),
                         border_width=0, bg_color="transparent", fg_color="transparent", hover_color="#6666ff",
                         height=40, width=300, corner_radius=10)
-k_medoid.place(y=235, x=0)
+k_medoid.place(y=190, x=0)
 
 # main loop
 root.mainloop()
